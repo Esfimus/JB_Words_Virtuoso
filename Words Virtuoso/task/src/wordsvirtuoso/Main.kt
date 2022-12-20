b35114109e4af3c21cbbc09059b1219a1d728a1d
Nikolay N. Nikolaev <esfimus@gmail.com>
2022-12-20 10:38:17 +0300

    Next stage completed

--- Words Virtuoso/task/src/wordsvirtuoso/Main.kt
+++ Words Virtuoso/task/src/wordsvirtuoso/Main.kt
@@ -1,11 +1,12 @@
 package wordsvirtuoso
 
 import java.io.File
+import kotlin.system.exitProcess
 
 class Words {
     private val wordsSet = mutableSetOf<String>()
     fun getWordsSet() = wordsSet
-    fun addWord(word: String) = wordsSet.add(word)
+    fun addWord(word: String) = wordsSet.add(word.lowercase())
 }
 
 /**
@@ -18,35 +19,54 @@ fun wordAnalysis(word: String): Boolean {
 }
 
 /**
- * Reads a file with words and checks if the words are valid
+ * Checks and adds correct words to object's set
  */
-fun wordsVirtuoso() {
-    lateinit var fileName: String
-    try {
-        // reading a file
-        println("Input the words file:")
-        fileName = readln()
-        val fileList = File(fileName).readLines()
-        val wordsCollection = Words()
-        var invalidWordsCount = 0
-        // analyzing every word
-        for (w in fileList) {
-            if (wordAnalysis(w)) {
-                wordsCollection.addWord(w)
-            } else {
-                invalidWordsCount++
-            }
-        }
-        if (invalidWordsCount == 0) {
-            println("All words are valid!")
+fun addWordsFromList(list: List<String>, wordsCollection: Words, fileName: String) {
+    var invalidWordsCount = 0
+    for (w in list) {
+        if (wordAnalysis(w)) {
+            wordsCollection.addWord(w)
         } else {
-            println("Warning: $invalidWordsCount invalid words were found in the $fileName file.")
+            invalidWordsCount++
         }
-    } catch (e: Exception) {
-        println("Error: The words file $fileName doesn't exist.")
+    }
+    if (invalidWordsCount != 0) {
+        println("Error: $invalidWordsCount invalid words were found in the $fileName file.")
+        exitProcess(0)
     }
 }
 
-fun main() {
-    wordsVirtuoso()
+/**
+ * Reads a file with words and checks if the words are valid
+ */
+fun wordsVirtuoso(args: Array<String>) {
+    val allWordsCollection = Words()
+    val candidateWordsCollection = Words()
+    val allWordsFile: List<String>
+    val candidateWordsFile: List<String>
+    if (args.size != 2) {
+        println("Error: Wrong number of arguments.")
+        exitProcess(0)
+    } else {
+        try { allWordsFile = File(args[0]).readLines() } catch (e: Exception) {
+            println("Error: The words file ${args[0]} doesn't exist.")
+            exitProcess(0)
+        }
+        try { candidateWordsFile = File(args[1]).readLines() } catch (e: Exception) {
+            println("Error: The candidate words file ${args[1]} doesn't exist.")
+            exitProcess(0)
+        }
+        addWordsFromList(allWordsFile, allWordsCollection, args[0])
+        addWordsFromList(candidateWordsFile, candidateWordsCollection, args[1])
+        val includedWords = candidateWordsCollection.getWordsSet() - allWordsCollection.getWordsSet()
+        if (includedWords.isNotEmpty()) {
+            println("Error: ${includedWords.size} candidate words are not included in the ${args[0]} file.")
+            exitProcess(0)
+        }
+        println("Words Virtuoso")
+    }
+}
+
+fun main(args: Array<String>) {
+    wordsVirtuoso(args)
 }
